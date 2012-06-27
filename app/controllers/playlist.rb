1ffffafbf674ec144adc2402202017ebb5e33279
Nat Welch <nat@natwelch.com>
2012-06-27 15:33:25 -0700

    Untested changes.

--- app/controllers/playlist.rb
+++ app/controllers/playlist.rb
@@ -1,12 +1,21 @@
 NatTape.controllers :playlist do
   layout :main
 
-  get :view, :with => :id do
+  get :new do
+    render :new
+  end
+
+  post :new do
 
   end
 
-  get :edit, :with => :id do
+  get :view, :with => :id do
+    @playlist = Playlist.where(:url => id)
+    render :view
+  end
 
+  get :edit, :with => :id do
+    render :view
   end
 
   post :edit, :with => :id do
