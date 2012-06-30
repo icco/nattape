480c92ba0cd1ab514b4be4d69b2b3d61d0cbc7ba
Nat Welch <nat@natwelch.com>
2012-06-29 21:43:23 -0700

    to store song data.

--- models/song.rb
+++ models/song.rb
@@ -2,6 +2,17 @@ class Song < ActiveRecord::Base
   belongs_to :playlist
   mount_uploader :file, SongUploader
   after_initialize :defaults
+  before_save :update_asset_attributes
+    
+  private
+
+  # TODO(icco): this.
+  def update_asset_attributes
+    #  if asset.present? && asset_changed?
+    #    self.content_type = asset.file.content_type
+    #    self.file_size = asset.file.size
+    #  end
+  end
 
   def defaults
     self.title ||= "No Title"
