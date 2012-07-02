68fe502c61ee444d596ffa820eee707d2b57515f
Nat Welch <nat@natwelch.com>
2012-07-02 12:53:53 -0700

    fix migration now that we aren't using paperclip.

--- db/migrate/20120627232445_add_attachment_to_songs.rb
+++ db/migrate/20120627232445_add_attachment_to_songs.rb
@@ -1,5 +1,8 @@
 class AddAttachmentToSongs < ActiveRecord::Migration
   def change
-    add_attachment :songs, :file
+    add_column :songs, "file_file_name", :string
+    add_column :songs, "file_content_type", :string
+    add_column :songs, "file_file_size", :string
+    add_column :songs, "file_updated_at", :string
   end
 end
